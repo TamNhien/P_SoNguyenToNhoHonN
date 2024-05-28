2bdb9692b24441380e8bbd032e265d180bfce9ce
TamNhien <152607459+TamNhien@users.noreply.github.com>
2024-05-28 20:01:34 +0700

    Update P_SoNguyenToNhoHonN.py

--- P_SoNguyenToNhoHonN.py
+++ P_SoNguyenToNhoHonN.py
@@ -15,11 +15,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-def is_prime(num):
-    if num < 2:
-        return False
-    return all(num % i != 0 for i in range(2, int(num**0.5) + 1))
-
-def main():
-    
